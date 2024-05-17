bcd8901d20b0813840b5f77c650ccd62b41a0c78
B302 <dev@b302.nl>
2024-05-17 13:56:27 +0200

    Backend V1.0.3

--- deployment/entrypoint.sh
+++ deployment/entrypoint.sh
@@ -2,6 +2,11 @@
 chmod -R 777 /var/www/html
 chown -R www-data:www-data /var/www/html
 
+# Ensure necessary NGINX directories exist and set permissions
+mkdir -p /var/cache/nginx/client_temp
+chown -R www-data:www-data /var/cache/nginx
+chmod -R 755 /var/cache/nginx
+
 php artisan migrate:fresh --seed
 php artisan scribe:generate
 php-fpm
